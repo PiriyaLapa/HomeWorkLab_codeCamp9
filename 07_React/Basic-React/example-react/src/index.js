c9fb611277c25a2384935b8461c8a4e45b13f88d
BenzDesktop <piriyalapa@gmail.com>
2021-09-17 11:27:23 +0700

    update

--- 07_React/Basic-React/example-react/src/index.js
+++ 07_React/Basic-React/example-react/src/index.js
@@ -1,36 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// // const element = <h1>Hello, world</h1>;
-// const element1 = React.createElement(
-//   'h1',
-//   { style: { color: 'green' } },
-//   'Resume'
-// );
-// const element2 = React.createElement(
-//   'h2',
-//   { style: { color: 'blue' } },
-//   'Piriya Lapa Codecamp#9'
-// );
-// const element3 = React.createElement(
-//   'p',
-//   { style: { color: 'navy' } },
-//   'I like green color, my height is 170 cm, my weigth is 68, The best preparation for tomorrow is doing your best today.'
-// );
-// const App = () => {
-//   return (
-//     <div>
-//       {element1}
-//       <br />
-//       {element2}
-//       <br />
-//       {element3}
-//     </div>
-//   );
-// };
+const myComponent = {
+  one: function () {
+    return <div>one function</div>;
+  },
+  two: function (prop) {
+    return <h2>{prop.name}</h2>;
+  },
+  three: function (component) {
+    return React.createElement('h1', null, { component });
+  },
+};
+
+const App = () => {
+  return (
+    <div title='Lab1'>
+      <ul>
+        <li>{<myComponent.one />}</li>
+        <li>{<myComponent.two name='piriya' />}</li>
+      </ul>
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
